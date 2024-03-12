396fb57da2be0a732669d5ddc50402bfce8f5d17
Darya-Shelaeva <selaevadarya03@gmail.com>
2024-03-12 20:32:19 +0300

    edit textarea

--- src/Components/Contact form/index.tsx
+++ src/Components/Contact form/index.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import './ContactForm.scss';
 
+interface IText{
+    text: string,
+}
+
+const text: IText = {
+    text: '',
+}
+
 const ContactForm = () => {
+    const [formValues, setFormValues] = useState('')
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setFormValues(e.target.value)
+    }
     return(
         <div className="form">
             <p className='form__write'>Напишите нам</p>
-            <textarea className="form__text"></textarea>
+            <textarea onChange={onChange} className="form__text" name="text" value={formValues}/>
             <button className="form__button">Отправить</button>
         </div>
     )
