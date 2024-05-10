10ecf47973eaa5a106ff876ab975540f578b83d2
Darya-Shelaeva <selaevadarya03@gmail.com>
2024-05-10 15:41:22 +0300

    adaptiv

--- src/components/ArtistPage/index.tsx
+++ src/components/ArtistPage/index.tsx
@@ -4,19 +4,30 @@ const ArtistPage = () => {
     return(
         <div className="artistContainer">
             <div className='artistContainer__name'>
-                <img src="/img/michaeljackson.jpg" alt="" />
-                <p>Michael Jackson</p>
+                <div className='artistContainer__name'>
+                    <div className='artistContainer__name__img'>
+                        <img src="/img/michaeljackson.jpg" alt="" />
+                    </div>  
+                    </div>
+                 <p className='artistContainer__name__n'>Michael Jackson</p>
             </div>
             <div className="artistContainer__name">
-                <img src="/img/michaeljackson.jpg" alt="" />
-                <p>Michael Jackson</p>
+            <div className='artistContainer__name__img'>
+                        <img src="/img/michaeljackson.jpg" alt="" />
+                    </div>  
+                <p className='artistContainer__name__n'>Michael Jackson</p>
             </div>
             <div className="artistContainer__name">
-                <img src="/img/michaeljackson.jpg" alt="" />
-                <p>Michael Jackson</p>
+            <div className='artistContainer__name__img'>
+                        <img src="/img/michaeljackson.jpg" alt="" />
+                    </div>  
+                <p className='artistContainer__name__n'>Michael Jackson</p>
             </div>
         </div>
     )
 }
+
+
+
 
 export default ArtistPage;
